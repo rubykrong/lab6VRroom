7638f800043b19e09347ea18c5af2279bc5d06b3
rubykrong <rubykrong@MacBook-Air-Ruby.local>
2017-01-13 18:48:18 +0300

    first commit

---
+++ Assets/MovePlayer.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePlayer : MonoBehaviour {
+
+	public GameObject player;
+	public Vector3[] targets;
+	public float speedWalk;
+	Vector3 point;
+	bool isWalk;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+		if (isWalk) {
+			Vector3 target = Vector3.MoveTowards (player.transform.position, point, speedWalk);
+
+			player.GetComponent<Transform> ().position = new Vector3 (target.x, player.transform.position.y, target.z);
+			Vector3 v = player.transform.position - point;
+			if(Mathf.Abs(v.x)<=speedWalk && Mathf.Abs(v.z) <= speedWalk)
+				isWalk = false;
+
+		}
+
+	}
+
+	public  void GoToPoint(int i)
+	{
+
+		point = targets [i];
+		isWalk = true;
+	}
+}
---
+++ Assets/Scripts/UIWallController.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIWallController : MonoBehaviour {
+
+	public RectTransform button0, button1, button2, button3, button4;
+	public Material mat1, mat2, mat3, mat4;
+	public GameObject wall;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+	}
+
+
+	public void ButtonMainEvent()
+	{
+		button0.gameObject.SetActive (false);
+		button1.gameObject.SetActive (true);
+		button2.gameObject.SetActive (true);
+		button3.gameObject.SetActive (true);
+		button4.gameObject.SetActive (true);
+	}
+
+	public void ButtonPick1Event()
+	{
+		wall.GetComponent<Renderer> ().material = mat1;
+		button0.gameObject.SetActive (true);
+		button1.gameObject.SetActive (false);
+		button2.gameObject.SetActive (false);
+		button3.gameObject.SetActive (false);
+		button4.gameObject.SetActive (false);
+	}
+
+	public void ButtonPick2Event()
+	{
+		wall.GetComponent<Renderer> ().material = mat2;
+		button0.gameObject.SetActive (true);
+		button1.gameObject.SetActive (false);
+		button2.gameObject.SetActive (false);
+		button3.gameObject.SetActive (false);
+		button4.gameObject.SetActive (false);
+	}
+
+	public void ButtonPick3Event()
+	{
+		wall.GetComponent<Renderer> ().material = mat3;
+		button0.gameObject.SetActive (true);
+		button1.gameObject.SetActive (false);
+		button2.gameObject.SetActive (false);
+		button3.gameObject.SetActive (false);
+		button4.gameObject.SetActive (false);
+	}
+
+	public void ButtonPick4Event()
+	{
+		wall.GetComponent<Renderer> ().material = mat4;
+		button0.gameObject.SetActive (true);
+		button1.gameObject.SetActive (false);
+		button2.gameObject.SetActive (false);
+		button3.gameObject.SetActive (false);
+		button4.gameObject.SetActive (false);
+	}
+}
